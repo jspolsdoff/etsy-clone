545b4da73a7b7109f3619e660205e77815ade485
James Spolsdoff <jspolsdoff@gmail.com>
2015-12-25 01:45:25 +0000

    added s3 storage and figaro

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem "paperclip", "~> 4.3"
+gem 'aws-sdk', '< 2.0'
+gem "figaro"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,13 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "missing.png"
+	has_attached_file :image, 
+										styles: { medium: "200x", thumb: "100x100>" }, 
+										default_url: "https://s3.amazonaws.com/kazijams/listings/missing.png",
+										:storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+  def s3_credentials
+    {:bucket => ENV["directory"], :access_key_id => ENV["aws_access_key_id"], :secret_access_key => ENV["aws_secret_access_key"]}
+  end
 end
